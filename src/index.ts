d61186189efa6e7fc0d462f9fe6379a39708e5d3
Yoyayee <eden555007@gmail.com>
2025-09-26 03:19:43 +0700

    fin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import morgan from 'morgan';
-
+import { Router, type Request, type Response } from "express";
+ 
+import routerc from "./routes/courseRoutes.js";
+import routers from "./routes/studentRoutes.js";
+ 
 const app: any = express();
 
 //Middleware
@@ -10,6 +14,38 @@ app.use(morgan('dev'));
 app.listen(3000, () =>
   console.log("🚀 Server running on http://localhost:3000")
 );
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "lab 15 API service successfully",
+  });
+});
+app.get("/me", (req: Request, res: Response) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Student Information",
+      data: {
+        studentId: "670610715",
+        firstName: "Pawornpruch",
+        lastName: "Sanlee",
+        program: "CPE",
+        section: "001",
+      },
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: error,
+    });
+  }
+});
+
+ app.use("/api/v2",routerc);
+ app.use("/api/v2",routers);
+
 
 
 export default app;
--- src/routes/courseRoutes.ts
+++ src/routes/courseRoutes.ts
@@ -1,21 +1,184 @@
-import { Router } from "express";
-const router: Router = Router();
+import { Router, type Request, type Response } from "express";
+ 
+import { courses  } from "../db/db.js";
+import {
+  zCourseDeleteBody,
+  zCourseId,
+  zCoursePostBody,
+  zCoursePutBody,
+} from "../schemas/courseValidator.js";
+import type { Course } from "../libs/types.js";
+const routerc: Router = Router();
 
 // READ all
-router.get("/", () => {
+routerc.get("/courses", (req: Request, res: Response) => {
+  try {
+    return res.status(200).json(courses);
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: error,
+    });
+  }
+});
+// Params URL
+routerc.get("/courses/:courseId", (req: Request, res: Response) => {
+  try {
+    const courseId = Number(req.params.courseId);
+    const result = zCourseId.safeParse(courseId);
+
+    if (!result.success) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: result.error.issues[0]?.message,
+      });
+    }
+
+    const findcourse = courses.findIndex(
+      (course) => course.courseId === courseId
+    );
+
+    if (findcourse === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Course does not exists",
+      });
+    }
+    res.set("Link", `/courses/${courseId}`);
+    return res.status(200).json({
+      success: true,
+      message: `Get course ${courseId} successfully`,
+      data: courses[findcourse],
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: error,
+    });
+  }
 });
 
-// Params URL 
-router.get("/", () => {
+routerc.post("/courses", (req: Request, res: Response) => {
+  try {
+    const body = req.body as Course;
+
+    const result = zCoursePostBody.safeParse(body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: result.error.issues[0]?.message,
+      });
+    }
+
+    const foundcourse = courses.find(
+      (course) => course.courseId === body.courseId
+    );
+
+    if (foundcourse) {
+      return res.status(409).json({
+        success: false,
+        message: "Course is already exists",
+      });
+    }
+    res.set("link", `/courses/${body.courseId}`);
+    courses.push(body);
+
+    return res.status(201).json({
+      success: true,
+      message: `Course ${body.courseId} has been added successfully` ,
+      data: {
+        body 
+      },
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: error,
+    });
+  }
 });
 
-router.post("/", () => {
+routerc.put("/courses", (req: Request, res: Response) => {
+  try {
+    const body = req.body as Course;
+    const result = zCoursePutBody.safeParse(body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: result.error.issues[0]?.message,
+      });
+    }
+
+    const foundcourse = courses.findIndex(
+      (course) => course.courseId === body.courseId
+    );
+
+    if (foundcourse === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Course Id is not exists",
+      });
+    }
+    res.set("link", `/courses/${body.courseId}`);
+    courses[foundcourse] = { ...courses[foundcourse], ...body };
+
+    return res.status(200).json({
+      success: true,
+      message: `course ${courses[foundcourse].courseId} has been updated successfully`,
+      data: courses[foundcourse],
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: error,
+    });
+  }
 });
 
-router.put("/", () => {
+routerc.delete("/courses", (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+
+    const result = zCourseDeleteBody.safeParse(body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: result.error.issues[0]?.message,
+      });
+    }
+
+    const foundcourse = courses.findIndex(
+      (course) => body.courseId === course.courseId
+    );
+
+    if (foundcourse === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Course Id is not exists",
+      });
+    }
+    const deletecourse = courses[foundcourse];
+    courses.splice(foundcourse, 1);
+    res.set("link", `/courses/${body.courseId}`);
+    return res.status(200).json({
+      success: true,
+      message: `Course ${deletecourse?.courseId} has been deleted successfully`,
+      data: deletecourse,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: error,
+    });
+  }
 });
 
-router.delete("/",() => {
-});
-
-export default router;
+export default routerc;
